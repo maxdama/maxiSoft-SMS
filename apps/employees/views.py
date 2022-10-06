88c54c21f8fb224911c255279917a88389388163
Administrator <maxdama@gmail.com>
2022-10-06 02:10:21 +0100

    Update as 06-10-2022 02:10am

--- apps/employees/views.py
+++ apps/employees/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
@@ -23,13 +24,19 @@ def list_employees(request):
 
 def employee_entry(req):
     """ New Employee Entry"""
+    print('employee_entry :- Function')
     emp_id = 0
     if req.method == 'POST':
+        print('---- Posting Form')
         employee = EmployeeForm(req.POST or None)
         if employee.is_valid():
-            employee.save(commit=False)
-            employee.staff_no = 'NGS-006'
+            print('----- Form is Valid')
+            emp = employee.save(commit=False)
+            emp.staff_no = 'NGS-006'
 
+            emp.save()
+
+            messages.info(req, 'New Employee Saved')
             return redirect("list-employees")
     else:
         context = {'header': 'Employee Entry', 'emp_id': emp_id}
