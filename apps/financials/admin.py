342b0b02a506968888b70d5259d3e78f026f17ed
Administrator <maxdama@gmail.com>
2022-12-04 01:04:18 +0100

    Update as 04-12-2022 07:39am

--- apps/financials/admin.py
+++ apps/financials/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from apps.financials.models import Enrollments
 
 # Register your models here.
+admin.site.register(Enrollments)
--- apps/students/admin.py
+++ apps/students/admin.py
@@ -3,4 +3,3 @@ from . models import  Students #, Enrollments
 # Register your models here.
 
 admin.site.register(Students)
-# admin.site.register(Enrollments)
